4a876f293fc5a8aa4e74a6000ec0ac2f4f7d528c
Ali Bayatpour <aliglbayatpour@gmail.com>
2020-04-29 13:37:34 +0300

    Updates

---
+++ precache-manifest.cc657ea7245c29ecc409fa7d5be4c6e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bce84b7e79fe6b9603b0edc2884d8e",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "f0148ed0231ec63c88cf",
+    "url": "/online-shop/static/css/main.c38a1c94.chunk.css"
+  },
+  {
+    "revision": "0711a6901c7f5c9e770d",
+    "url": "/online-shop/static/js/2.9f7546a8.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/online-shop/static/js/2.9f7546a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0148ed0231ec63c88cf",
+    "url": "/online-shop/static/js/main.4e92ae2f.chunk.js"
+  },
+  {
+    "revision": "874209477cfa272ebcbe",
+    "url": "/online-shop/static/js/runtime-main.f91e04bd.js"
+  },
+  {
+    "revision": "ec65ffb8132aa426078068ef675a79e5",
+    "url": "/online-shop/static/media/bag.ec65ffb8.svg"
+  },
+  {
+    "revision": "75bab4fd6bdd96bc5b5528983ba7b20a",
+    "url": "/online-shop/static/media/banner.75bab4fd.jpg"
+  },
+  {
+    "revision": "2793518b35b585bdcd119fd104248925",
+    "url": "/online-shop/static/media/logo.2793518b.webp"
+  }
+]);
