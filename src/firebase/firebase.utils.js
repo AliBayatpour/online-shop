138fd5e97cb48dce3255366bfd0ec9b8a27b1037
AliBayatpour <aliglbayatpour@gmail.com>
2020-05-02 16:16:36 +0300

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,17 +2,6 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyCoF70Q8YbZyCL1PlEvN3Js5esnWpe47Ck",
-  authDomain: "online-shop-fadfc.firebaseapp.com",
-  databaseURL: "https://online-shop-fadfc.firebaseio.com",
-  projectId: "online-shop-fadfc",
-  storageBucket: "online-shop-fadfc.appspot.com",
-  messagingSenderId: "745182097708",
-  appId: "1:745182097708:web:65b46d000fd374e5c2fc89",
-  measurementId: "G-33HWCBK7N1",
-};
-
 firebase.initializeApp(config);
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
